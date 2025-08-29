c5ff446a85040a29530627709daecd68efa7ed09
ernesto-1998 <ernestoalonso1998@gmail.com>
2025-08-29 00:34:38 -0600

    Reducing boilerplate code by implementing OmitType and PartialType on CRUD User Dtos

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,60 +1,9 @@
-import { Type } from 'class-transformer';
-import {
-  IsEmail,
-  IsOptional,
-  IsString,
-  MinLength,
-  MaxLength,
-  IsNotEmpty,
-  ValidateNested,
-} from 'class-validator';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { ProfileDto } from './profile.dto';
+import { OmitType } from '@nestjs/swagger';
+import { UserDto } from './user.dto';
 
-export class CreateUserDto {
-  @ApiProperty({
-    example: 'user@example.com',
-    description: 'Unique email of the user',
-  })
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
-
-  @ApiProperty({
-    example: 'johndoe',
-    description: 'Unique username of the user',
-  })
-  @IsString()
-  @IsNotEmpty()
-  username: string;
-
-  @ApiProperty({
-    example: 'strongPass123',
-    minLength: 8,
-    maxLength: 20,
-    description: 'User password (min 8, max 20 characteres)',
-  })
-  @IsString()
-  @MinLength(8, { message: 'The min length of password is 8' })
-  @MaxLength(20, {
-    message: 'The password can not accept more than 20 characters',
-  })
-  password: string;
-
-  @ApiProperty({
-    example: 'admin',
-    description: 'Users roles (e.g. user, admin)',
-  })
-  @IsString()
-  @IsNotEmpty()
-  role: string;
-
-  @ApiPropertyOptional({
-    type: () => ProfileDto,
-    description: 'Users Profile (optional)',
-  })
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => ProfileDto)
-  profile?: ProfileDto;
-}
+export class CreateUserDto extends OmitType(UserDto, [
+  '_id',
+  'createdAt',
+  'updatedAt',
+  '__v',
+] as const) {}
